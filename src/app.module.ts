8d903aa920960aa7a2f3879aeab81cc2645892ff
oussamaB14 <88203283+oussamaB14@users.noreply.github.com>
2024-05-09 09:29:29 +0100

    cpmmit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://oussamaboufari:qrKSIr01IOshnkQE@cluster0.xtkavhf.mongodb.net/?retryWrites=true&w=majority'), ProductModule, UserModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://oussamaboufari:qrKSIr01IOshnkQE@cluster0.xtkavhf.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
